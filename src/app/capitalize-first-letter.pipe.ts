b995e15f0664e1535ae8624a32e99c61c7fe86c8
Dor <earstuff@gmail.com>
2018-03-20 20:26:46 +0200

    fixed validation pipe

--- src/app/capitalize-first-letter.pipe.ts
+++ src/app/capitalize-first-letter.pipe.ts
@@ -1,15 +1,21 @@
 import {Pipe, PipeTransform} from '@angular/core';
 
+function titleCaseWord(word: string) {
+  if (!word) return word;
+  return word[0].toUpperCase() + word.substr(1).toLowerCase();
+}
+
 @Pipe({
   name: 'capitalizeFirst'
 })
+
 export class CapitalizeFirstPipe implements PipeTransform {
   transform(value: string): string {
     if (value === null) {
       return 'Not assigned';
     }
     const newVal = value.replace(/[-!$%@^&*()_+|~=`{}\[\]:";<>?,.\/]/g, '');
-    return newVal.charAt(0).toUpperCase() + newVal.slice(1);
+    return newVal.split(/\b/g).map(word => titleCaseWord(word)).join('');
   }
 }
 
